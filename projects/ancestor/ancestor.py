fa91f2d2c20e66d85a80e32f003fdb33814f6059
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-11-05 19:23:53 +0100

    implement ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -4,24 +4,25 @@ from util import Stack
 
 def earliest_ancestor(ancestors, starting_node):
     graph = Graph()
-    graph.add_vertex(1)
-    graph.add_vertex(2)
-    graph.add_vertex(3)
-    graph.add_vertex(4)
-    graph.add_vertex(5)
-    graph.add_vertex(6)
-    graph.add_vertex(7)
-    graph.add_vertex(8)
-    graph.add_vertex(9)
-    graph.add_vertex(10)
-    graph.add_vertex(11)
+    for i in range(12):
+        graph.add_vertex(i)
+    # graph.add_vertex(1)
+    # graph.add_vertex(2)
+    # graph.add_vertex(3)
+    # graph.add_vertex(4)
+    # graph.add_vertex(5)
+    # graph.add_vertex(6)
+    # graph.add_vertex(7)
+    # graph.add_vertex(8)
+    # graph.add_vertex(9)
+    # graph.add_vertex(10)
+    # graph.add_vertex(11)
     for elem in ancestors:
         graph.add_edge(elem[1], elem[0])
-    # print(graph.vertices)
     s = Stack()
 
     s.push(starting_node)
-    visited_path = [] # []
+    visited_path = [] 
    
     while s.size() > 0:
         depoped = s.pop()
@@ -36,7 +37,7 @@ def earliest_ancestor(ancestors, starting_node):
     elif len(visited_path) == 1:
         for elem in visited_path:
             return elem
-    return list(visited_path)[len(visited_path) - 1]
+    return visited_path[len(visited_path) - 1]
 
 
 print(earliest_ancestor([(1, 3), (2, 3), (3, 6), (5, 6),
